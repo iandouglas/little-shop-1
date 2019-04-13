230b8c12840499b397022135b7b6bab415854cfa
Matt Weiss <weiss.matt@outlook.com>
2019-04-13 15:33:22 -0600

    tests for slug not changing when email not changed

--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'As a registered user' do
                       email_address: "Jeremy@test_user.com",
                       password: "password",
                       enabled: true,
-                      slug: "Jeremy-test_user-com")
+                      slug: "jeremy-test_user-com")
 
   end
   it "I can edit my profile information" do
@@ -49,6 +49,42 @@ RSpec.describe 'As a registered user' do
     click_link "#{@user.name}"
 
     expect(current_path).to eq("/admin/users/changeo-changed_address-com")
+  end
+
+  it 'the slug will not change if I do not change my email' do
+    visit root_path
+
+    click_link "Log In"
+
+    fill_in "email_address", with: "#{@user.email_address}"
+    fill_in "password", with: "#{@user.password}"
+
+    click_button "Log Me In"
+
+    click_link "Edit Profile"
+
+    expect(current_path).to eq(edit_user_path(@user))
+    fill_in "Name", with: "Matt"
+
+    click_button "Update Account"
+
+    expect(current_path).to eq(profile_path)
+    expect(page).to have_content("Matt")
+
+    click_link "Log Out"
+
+    #checking for slug change
+    click_link "Log In"
+
+    fill_in "email_address", with: "#{@uadmin.email_address}"
+    fill_in "password", with: "#{@uadmin.password}"
+
+    click_button "Log Me In"
+
+    click_link "Users"
+    click_link "Matt"
+
+    expect(current_path).to eq("/admin/users/jeremy-test_user-com")
   end
 
   it 'throws error when any validations fail' do
